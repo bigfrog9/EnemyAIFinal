cba73468ec63131e89ace49e11f317cee9e4b7e5
John Wohler <johndoubleyou11@gmail.com>
2023-03-10 00:59:45 -0500

    Finished almost everything aside from the Searching method

--- Assets/State.cs
+++ Assets/State.cs
@@ -1,6 +1,9 @@
 using StarterAssets;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -25,6 +28,8 @@ public class State : MonoBehaviour
 
     public Vector3 PlayerPos;
 
+    public bool Seen = false;
+
     public enum CURRENTSTATE
     { PATROL, CHASE, ATTACK, SEARCH, RETREAT };
 
@@ -32,6 +37,8 @@ public class State : MonoBehaviour
     { goalOne, goalTwo };
 
     public float speed;
+
+    public float timer=0;
 
     CURRENTSTATE CurrentState = CURRENTSTATE.PATROL;
 
@@ -52,6 +59,11 @@ public class State : MonoBehaviour
 // Update is called once per frame
     void Update()
     {
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+        }
+
         if (CurrentState==CURRENTSTATE.PATROL)
         {
             runPatrol();
@@ -83,6 +95,13 @@ public class State : MonoBehaviour
     //travelling between two points
     public void runPatrol()
     {
+        gameObject.GetComponent<Renderer>().material.color = Color.green;
+
+        if (Seen)
+        {
+            CurrentState = CURRENTSTATE.CHASE;
+        }
+
         if (Goal==PATROLGOAL.goalOne)
         {
             enemy.SetDestination(Point1);
@@ -112,6 +131,7 @@ public class State : MonoBehaviour
         {
             detectsPlayer = true;
         }
+
         //controller._speed >= 1f &&
         //checking if the player is moving, I had to make _speed from FirstPersonController public
         if (controller.currentSpeed>=1.0f && Vector3.Distance(Player.transform.position, enemy.transform.position) <= 5)
@@ -128,26 +148,102 @@ public class State : MonoBehaviour
     //travelling after player
     public void runChase()
     {
+        gameObject.GetComponent<Renderer>().material.color = Color.red;
 
         enemy.SetDestination(Player.transform.position);
+
+        if (Vector3.Distance(Player.transform.position, enemy.transform.position) >= 10)
+        {
+            CurrentState = CURRENTSTATE.SEARCH;
+
+            //add player's last known location here
+        }
+
+        if (Vector3.Distance(Player.transform.position, enemy.transform.position) <= 2)
+        {
+            CurrentState=CURRENTSTATE.ATTACK;
+
+            timer = 3;
+        }
     }
 
     //stopping and attacking player
     public void runAttack()
     {
+        gameObject.GetComponent<Renderer>().material.color = Color.blue;
+
+        if (timer <= 0)
+        {
+            CurrentState=CURRENTSTATE.CHASE;
+        }
 
     }
 
     //going to last known player location and spinning for a while
     public void runSearch()
     {
+        gameObject.GetComponent<Renderer>().material.color = Color.yellow;
 
+
+        if (Seen)
+        {
+            CurrentState = CURRENTSTATE.CHASE;
+        }
+
+        if (Vector3.Distance(Player.transform.position, enemy.transform.position) <= 2)
+        {
+            detectsPlayer = true;
+        }
+
+        if (controller.currentSpeed >= 1.0f && Vector3.Distance(Player.transform.position, enemy.transform.position) <= 5)
+        {
+            detectsPlayer = true;
+        }
+
+        if (controller.currentSpeed >= 4.5f && Vector3.Distance(Player.transform.position, enemy.transform.position) <= 8)
+        {
+            detectsPlayer = true;
+        }
     }
 
     //returning to nearest patrol point
     public void runRetreat()
     {
+        gameObject.GetComponent<Renderer>().material.color = Color.white;
 
+        enemy.SetDestination(Point1);
+        
+        if (Vector3.Distance(Point1, enemy.transform.position) <= 0.5)
+        {
+            CurrentState=CURRENTSTATE.PATROL;
+        }
+
+        if (Seen)
+        {
+            CurrentState = CURRENTSTATE.CHASE;
+        }
+
+        if (Vector3.Distance(Player.transform.position, enemy.transform.position) <= 2)
+        {
+            detectsPlayer = true;
+        }
+
+        if (controller.currentSpeed >= 1.0f && Vector3.Distance(Player.transform.position, enemy.transform.position) <= 5)
+        {
+            detectsPlayer = true;
+        }
+
+        if (controller.currentSpeed >= 4.5f && Vector3.Distance(Player.transform.position, enemy.transform.position) <= 8)
+        {
+            detectsPlayer = true;
+        }
     }
 
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other == Player)
+        {
+            Seen = true;
+        }
+    }
 }
